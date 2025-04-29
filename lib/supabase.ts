479fafbf9e514c492524fecc8bfb6eeb53bf3a99
Auraplus5 <kevin.alak@ut.ee>
2025-04-29 20:05:31 +0300

    Update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,10 @@
 import { createClient } from "jsr:@supabase/supabase-js@2";
 
 
-const supabase_url=Deno.env.get("SUPABASE_URL");
-const supabase_key=Deno.env.get("SUPABASE_ANON_KEY");
+const supabase_url=Deno.env.get("SUPABASE_URL")!;
+const supabase_key=Deno.env.get("SUPABASE_ANON_KEY")!;
 
-if (!supabase_url || !supabase_key) {
-    throw new Error("Missing SUPABASE_URL or SUPABASE_ANON_KEY in .env");
-}
+
 
 const supabase = createClient(supabase_url, supabase_key);
 
