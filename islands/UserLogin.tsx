0671d7d6e5d1028afdacc23a45cdec0b8e9a9947
Auraplus5 <kevin.alak@ut.ee>
2025-05-05 11:28:22 +0300

    update

--- islands/UserLogin.tsx
+++ islands/UserLogin.tsx
@@ -36,6 +36,7 @@ export default function UserLogin( props: {
             return;
         } else {
             setMessage("Sisselogimine õnnestus!");
+
             //globalThis.location.href = "/";
         }
     };
